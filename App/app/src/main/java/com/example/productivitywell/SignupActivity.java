44589c6b4b2e8d4e59c7a1caa346c6049e428cd2
Nathan Jose <nathan_jose@berea.edu>
2020-04-12 19:27:51 -0400

    Fixed.

---
+++ App/app/src/main/java/com/example/productivitywell/SignupActivity.java
@@ -0,0 +1,77 @@
+package com.example.productivitywell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "SignupActivity";
+    private EditText etEmail;
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        if (ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+        etEmail = findViewById(R.id.etEmail);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick SignUp button");
+                String email = etEmail.getText().toString();
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signUpUser(email,username,password);
+            }
+        });
+    }
+
+    private void signUpUser(String email, String username, String password){
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with signUp",e);
+                    return;
+
+                }
+                goMainActivity();
+                Toast.makeText(SignupActivity.this, "success signing up!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
