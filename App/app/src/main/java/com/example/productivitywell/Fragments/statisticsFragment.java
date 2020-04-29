6b67b7b34d185f30fe3184a339798ccbc7e48304
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-04-29 06:13:42 -0400

    Added Fragemnts xmls

--- App/app/src/main/java/com/example/productivitywell/Fragments/statisticsFragment.java
+++ App/app/src/main/java/com/example/productivitywell/Fragments/statisticsFragment.java
@@ -1,27 +1,21 @@
 package com.example.productivitywell.Fragments;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.os.Bundle;
-import android.view.MenuItem;
+import com.example.productivitywell.Post;
 import com.example.productivitywell.R;
+import com.example.productivitywell.Statsdata;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
@@ -30,32 +24,61 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import lecho.lib.hellocharts.model.ColumnChartData;
-import lecho.lib.hellocharts.model.Line;
-import lecho.lib.hellocharts.model.LineChartData;
-import lecho.lib.hellocharts.model.PieChartData;
-import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.model.SliceValue;
-import lecho.lib.hellocharts.view.ColumnChartView;
-import lecho.lib.hellocharts.view.LineChartView;
-import lecho.lib.hellocharts.view.PieChartView;
-
-
 //
-
 public class statisticsFragment extends Fragment {
+    public static final String TAG = "Statistics Fragemnt";
+
+    public int focusTime =0;
+    int studyTime;
+    int sleepTime;
+    int workTime;
+    int other;
+    ArrayList<PieEntry> dataVals = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
+
+//    public void queryStats(){
+//        ParseQuery <Statsdata> query = ParseQuery.getQuery(Statsdata.class);
+//        query.findInBackground(new FindCallback<Statsdata>() {
+//            @Override
+//            public void done(List<Statsdata> datas, ParseException e) {
+//                if (e != null){
+//                    Log.e(TAG, " Issue with Login",e );
+//                    return;
+//                }
+//                int num  = 1;
+//                for (Statsdata data: datas){
+//                    if (num <2) {
+//                        focusTime = data.getFocusTime();
+//                        Log.i(TAG, "heree" + focusTime);
+////                  focusTime = data.getFocusTime();
+////                  sleepTime = data.getSleepTime();
+////                  studyTime = data.getStudyTime();
+////                  workTime = data.getWorkTime();
+////                  other = data.getOtherTime();
+//                    }
+//                    num +=1;
+//
+//                }
+//            }
+//        });
+//
+//
+//    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -86,17 +109,13 @@ public class statisticsFragment extends Fragment {
 
     public void addBarChart(BarChart barChart){
         String[] labels = new String[]{"Sun","Mon","Tues","Wed", "Thu","Fri","Sat"};
-
         BarDataSet barDataset1 = new BarDataSet(dataValues1(), "Dataset 1");
-
         BarData barData = new BarData();
         barData.addDataSet(barDataset1);
-
         barChart.setDrawValueAboveBar(false);
         barChart.setData(barData);
         barChart.getAxisLeft().setDrawAxisLine(false);
         barChart.getDescription().setEnabled(false);
-
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
         xAxis.setCenterAxisLabels(false);
@@ -125,21 +144,54 @@ public class statisticsFragment extends Fragment {
         pieChart.setUsePercentValues(false);
         pieChart.invalidate();
     }
+        public void getData(){
 
-    private ArrayList<PieEntry> dataValues2() {
-        ArrayList<PieEntry> dataVals = new ArrayList<>();
+        }
 
-        dataVals.add(new PieEntry(10,"focus"));
-        dataVals.add(new PieEntry(5,"study"));
-        dataVals.add(new PieEntry(40,"sleep"));
-        dataVals.add(new PieEntry(25,"work"));
-        dataVals.add(new PieEntry(20,"other"));
 
-        return dataVals;
+
+
+    public ArrayList<PieEntry> dataValues2() {
+        ParseQuery <Statsdata> query = ParseQuery.getQuery(Statsdata.class);
+        query.findInBackground(new FindCallback<Statsdata>() {
+            @Override
+
+
+            public void done(List<Statsdata> datas, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, " Issue with Login",e );
+                    return;
+                }
+                int num  = 1;
+                for (Statsdata data: datas){
+                    if (num <2) {
+                        dataVals.add(new PieEntry(2,"focus"));
+                        Log.i(TAG, "in the place" + data.getFocusTime());
+                        dataVals.add(new PieEntry(studyTime,"study"));
+                        dataVals.add(new PieEntry(sleepTime,"sleep"));
+                        dataVals.add(new PieEntry(workTime,"work"));
+                        dataVals.add(new PieEntry(other,"other"));
+                        focusTime = data.getFocusTime();
+
+//                  focusTime = data.getFocusTime();
+//                  sleepTime = data.getSleepTime();
+//                  studyTime = data.getStudyTime();
+//                  workTime = data.getWorkTime();
+//                  other = data.getOtherTime();
+
+                    }
+                    num +=1;
+
+                }
+
+            }
+
+        });
+    return dataVals;
     }
 
 
-    private ArrayList<BarEntry> dataValues1() {
+    public ArrayList<BarEntry> dataValues1() {
         ArrayList<BarEntry> dataVals = new ArrayList<>();
         dataVals.add(new BarEntry(0,3));
         dataVals.add(new BarEntry(1,4));
@@ -150,4 +202,8 @@ public class statisticsFragment extends Fragment {
         dataVals.add(new BarEntry(6,4));
         return dataVals;
     }
+
+
+
+
 }
