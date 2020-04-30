d19c0cf3b3751c48d15b94cdb6740e6cfbaa0c70
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-04-29 22:19:32 -0400

    Finished the data stuff.

--- App/app/src/main/java/com/example/productivitywell/Statsdata.java
+++ App/app/src/main/java/com/example/productivitywell/Statsdata.java
@@ -6,19 +6,23 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.util.Date;
+
 @ParseClassName("Stats")
 public class Statsdata extends ParseObject {
     public  final String FOCUS_TIME = "focus";
     public  final String  STUDY_TIME = "study";
     public  final  String SLEEP_TIME = "sleep";
     public  final String WORK_TIME = "work";
-    public  final String OTHER_TIME = "eat";
-    public  final String KEY_USER = "user";
+    public  final String OTHER_TIME = "other";
+    public  static final String KEY_USER = "user";
+    public final String DATE_USED = "dateUsed";
 
  // remember to set the setters for this class
     public int getFocusTime(){
         return getInt(FOCUS_TIME);
     }
+
 
     public int getStudyTime(){
         return getInt(STUDY_TIME);
@@ -36,5 +40,8 @@ public class Statsdata extends ParseObject {
 
     public ParseUser getUser(){
         return getParseUser(KEY_USER);
+    }
+    public Date gettDate(){
+        return getDate(DATE_USED);
     }
 }
